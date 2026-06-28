07b55aab627a2296d6eebbd83566b797030a0328
ZEBIRI MOUNDER <zbyrymndhr@gmail.com>
2026-06-29 00:24:26 +0200

    Update Law-of-Large-Numbers.R

--- Law-of-Large-Numbers.R
+++ Law-of-Large-Numbers.R
@@ -1,5 +1,5 @@
 x<-sample(1:6, 5000, replace = TRUE)
 m<-cumsum(x)/ 1:5000
 plot(m, type = "l", col="blue", main = "Law of Large Numbers")
-abline(h=3,5, col="red", lwd=2)
+abline(h=3.5, col="red", lwd=2)
 
